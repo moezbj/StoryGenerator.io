c56cf6bddc5253fb98e3967055287b1cb7b69647
Bouallagui Moncef <moncef.bouallagui@gmail.com>
2017-10-29 15:33:57 +0100

    Update story.js

--- story.js
+++ story.js
@@ -6,9 +6,16 @@ function randomValueFromArray(array){
   return array[Math.floor(Math.random()*array.length)];
 }
 
-var storyText = "It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
-var insertx = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-var inserty = ["the soup kitchen", "Disneyland", "the White House"];
+var storyText = 'It was 94 farenheit outside, so :insertx: went for a walk.' +
+	'When they got to :inserty:, they stared in horror for a few moments,' +
+	'then :insertz:. Bob saw the whole thing, but he was not surprised —' + 
+	':insertx: weighs 300 pounds, and it was a hot day.';
+
+var insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+var insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+var insertZ = ['spontaneously combusted',
+	'melted into a puddle on the sidewalk',
+	'turned into a slug and crawled away'];
 var insertz = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
 randomize.addEventListener('click', result);
